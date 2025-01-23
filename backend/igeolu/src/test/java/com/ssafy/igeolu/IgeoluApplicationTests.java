940bc94e612517819aabba6daf17baaf821e03f3
JINHOON <g22203@naver.com>
2025-01-23 13:40:59 +0900

    feat: 백엔드 프로젝트 세팅 [S12P11D205-153]

---
+++ backend/igeolu/src/test/java/com/ssafy/igeolu/IgeoluApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.igeolu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgeoluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
