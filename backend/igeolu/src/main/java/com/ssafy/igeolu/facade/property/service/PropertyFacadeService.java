8e24f71ca736c5cfe31cf01863a84cdf3690be88
조대성 <tfer2442@naver.com>
2025-02-02 22:00:16 +0900

    Revert "Merge branch 'fix/S12P11D205-201-spring-boot-openvidu' into 'fe'"
    This reverts merge request !20

--- backend/igeolu/src/main/java/com/ssafy/igeolu/facade/property/service/PropertyFacadeService.java
+++
@@ -1,4 +0,0 @@
-package com.ssafy.igeolu.facade.property.service;
-
-public interface PropertyFacadeService {
-}
--- backend/igeolu/src/main/java/com/ssafy/igeolu/facade/property/service/PropertyFacadeServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.igeolu.facade.property.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class PropertyFacadeServiceImpl implements PropertyFacadeService {
-}
