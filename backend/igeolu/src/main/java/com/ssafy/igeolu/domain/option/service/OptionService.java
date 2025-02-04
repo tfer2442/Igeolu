f906e7a85cbda9e195860dd6bc35c1351dace5c2
최재영 <horizontal4@naver.com>
2025-02-04 16:17:22 +0900

    git commit -m 'feat: 옵션 리스트 조회 기능 추가[S12P11D205-241]'

--- backend/igeolu/src/main/java/com/ssafy/igeolu/domain/option/service/OptionService.java
+++ backend/igeolu/src/main/java/com/ssafy/igeolu/domain/option/service/OptionService.java
@@ -7,7 +7,9 @@ import com.ssafy.igeolu.domain.propertyOption.entity.PropertyOption;
 
 public interface OptionService {
 	
-	// 옵션 조회
+	// 매물별 옵션 조회
 	List<Option> getOptionFromPropertyOptions(List<PropertyOption> propertyOptions);
 
+	// 옵션 리스트 조회
+	List<Option> getOptionList();
 }
