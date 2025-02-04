1a3020bbfa1b293537bb347d43e07027f721aa64
조대성 <tfer2442@naver.com>
2025-02-04 10:26:07 +0900

    feature: swagger api 인가 처리

--- backend/igeolu/src/main/java/com/ssafy/igeolu/global/config/SecurityConfig.java
+++ backend/igeolu/src/main/java/com/ssafy/igeolu/global/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
 		//경로별 인가 작업
 		http
 			.authorizeHttpRequests((auth) -> auth
-				.requestMatchers("/api/**").permitAll()
+				.requestMatchers("/api/**", "/v3/api-docs/**").permitAll()
 				.anyRequest().authenticated());
 
 		//세션 설정 : STATELESS
