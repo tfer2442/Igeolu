caa8f2c1fc7432d6b002b8ec5bb04609b35626dc
조대성 <tfer2442@naver.com>
2025-02-11 21:54:49 +0900

    feat: 평점 매길 수 있는 유저인지 확인하는 api  [S12P11D205-301]

--- backend/igeolu/src/main/java/com/ssafy/igeolu/presentation/rating/RatingController.java
+++ backend/igeolu/src/main/java/com/ssafy/igeolu/presentation/rating/RatingController.java
@@ -1,12 +1,14 @@
 package com.ssafy.igeolu.presentation.rating;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.igeolu.facade.rating.dto.request.RatingPostRequestDto;
+import com.ssafy.igeolu.facade.rating.dto.response.RatingEligibilityGetResponseDto;
 import com.ssafy.igeolu.facade.rating.service.RatingFacadeService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -30,5 +32,20 @@ public class RatingController {
 		ratingFacadeService.registerRating(liveId, ratingPostRequestDto);
 
 		return ResponseEntity.ok().build();
+	}
+
+	@Operation(
+		summary = "평점 등록 가능 여부 조회",
+		description = "라이브 종료 후, 현재 사용자가 평점 모달창을 띄울 자격(최초 참여자 여부 등)이 있는지 확인"
+	)
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "정상 처리")
+	})
+	@GetMapping("/api/lives/{liveId}/rating/eligibility")
+	public ResponseEntity<RatingEligibilityGetResponseDto> checkRatingEligibility(@PathVariable String liveId) {
+		RatingEligibilityGetResponseDto ratingEligibilityGetResponseDto = ratingFacadeService.checkRatingEligibility(
+			liveId);
+
+		return ResponseEntity.ok(ratingEligibilityGetResponseDto);
 	}
 }
