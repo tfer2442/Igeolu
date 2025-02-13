9b9ae16337012d1a48a5bca67d55f861a6494d14
조대성 <tfer2442@naver.com>
2025-02-13 10:11:06 +0900

    feat: 알림 Entity 및 DTO 수정 [S12P11D205-312]

--- backend/igeolu/src/main/java/com/ssafy/igeolu/facade/notification/dto/response/AppointmentNotificationResponseDto.java
+++ backend/igeolu/src/main/java/com/ssafy/igeolu/facade/notification/dto/response/AppointmentNotificationResponseDto.java
@@ -18,12 +18,9 @@ public class AppointmentNotificationResponseDto {
 	@Schema(description = "스케줄 날짜", example = "yyyy-MM-dd(E) HH:mm")
 	private String scheduledAt;
 
+	@Schema(description = "알림 발생일")
+	private String createdAt;
+
 	@Schema(description = "알림 내용")
 	private String message;
-
-	@Schema(description = "약속 상대방 id")
-	private Integer opponentId;
-
-	@Schema(description = "약속 상대방 이름")
-	private String opponentName;
 }
