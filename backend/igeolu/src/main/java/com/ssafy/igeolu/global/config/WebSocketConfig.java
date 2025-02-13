53082f0fc3dd46cde048978e45025cc4c946ebe8
조대성 <tfer2442@naver.com>
2025-02-14 00:29:31 +0900

    feat: 커스텀 taskscheduler 등록

--- backend/igeolu/src/main/java/com/ssafy/igeolu/global/config/WebSocketConfig.java
+++ backend/igeolu/src/main/java/com/ssafy/igeolu/global/config/WebSocketConfig.java
@@ -3,6 +3,7 @@ package com.ssafy.igeolu.global.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -18,7 +19,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	private final JWTUtil jwtUtil;
-	private final TaskScheduler taskScheduler;
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -37,8 +37,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 		// 메시지를 구독(수신)하는 요청 엔드포인트
 		// /api/sub 로 시작하는 stomp 메세지는 브로커로 라우팅함
 		registry.enableSimpleBroker("/api/sub", "/api/sub-user")
-			.setHeartbeatValue(new long[] {10000, 10000}) // 10초 간격
-			.setTaskScheduler(taskScheduler);
+			.setTaskScheduler(taskScheduler())
+			.setHeartbeatValue(new long[] {10000, 10000}); // 10초 간격
 		// 첫 번째 값: 서버가 클라이언트로 heartbeat를 보내는 간격
 		// 두 번째 값: 클라이언트가 서버로 heartbeat를 보내는 간격
 
@@ -48,5 +48,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 		// 사용자 전용 메시지 전송을 위한 접두사 설정 (예: convertAndSendToUser() 사용 시)
 		registry.setUserDestinationPrefix("/api/sub-user");
+	}
+
+	public TaskScheduler taskScheduler() {
+		ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+		taskScheduler.initialize();
+		return taskScheduler;
 	}
 }
