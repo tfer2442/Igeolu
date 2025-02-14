214f73f8e98abe7759c4c0168c86412f9a5c4211
조대성 <tfer2442@naver.com>
2025-02-14 23:10:11 +0900

    feat: 약속 반환시 타입 추가 [S12P11D205-333]

--- backend/igeolu/src/main/java/com/ssafy/igeolu/facade/appointment/dto/response/AppointmentPostResponseDto.java
+++ backend/igeolu/src/main/java/com/ssafy/igeolu/facade/appointment/dto/response/AppointmentPostResponseDto.java
@@ -1,5 +1,8 @@
 package com.ssafy.igeolu.facade.appointment.dto.response;
 
+import com.ssafy.igeolu.domain.appointment.entity.AppointmentType;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,5 +11,9 @@ import lombok.Data;
 @Builder
 @AllArgsConstructor
 public class AppointmentPostResponseDto {
+	@Schema(description = "약속 id")
 	Integer appointmentId;
+
+	@Schema(description = "약속 타입", example = "LIVE", allowableValues = {"LIVE", "COMMON"})
+	AppointmentType appointmentType;
 }
