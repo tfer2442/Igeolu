a64b55bdf12731519012d7a64db317d370fa9e2f
박진훈 <g22203@naver.com>
2025-02-17 22:55:31 +0900

    feat: 채팅방 나가기 반환값 제거 [S12P11D205-357]

--- backend/igeolu/src/main/java/com/ssafy/igeolu/presentation/chatroom/controller/ChatRoomController.java
+++ backend/igeolu/src/main/java/com/ssafy/igeolu/presentation/chatroom/controller/ChatRoomController.java
@@ -1,62 +1,53 @@
 package com.ssafy.igeolu.presentation.chatroom.controller;
 
-import java.util.List;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.ssafy.igeolu.facade.chatroom.dto.request.ChatRoomListGetRequestDto;
 import com.ssafy.igeolu.facade.chatroom.dto.request.ChatRoomPostRequestDto;
 import com.ssafy.igeolu.facade.chatroom.dto.response.ChatRoomListGetResponseDto;
 import com.ssafy.igeolu.facade.chatroom.dto.response.ChatRoomPostResponseDto;
 import com.ssafy.igeolu.facade.chatroom.service.ChatRoomFacadeService;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/chats")
 public class ChatRoomController {
 
-	private final ChatRoomFacadeService chatRoomFacadeService;
+    private final ChatRoomFacadeService chatRoomFacadeService;
 
-	@Operation(summary = "채팅방 생성", description = "채팅방을 생성합니다.")
-	@ApiResponses(value = {
-		@ApiResponse(responseCode = "200", description = "정상 처리"),
-	})
-	@PostMapping("")
-	public ResponseEntity<ChatRoomPostResponseDto> createChatRoom(
-		@RequestBody ChatRoomPostRequestDto request) {
-		ChatRoomPostResponseDto response = chatRoomFacadeService.createChatRoom(request);
-		return ResponseEntity.ok().body(response);
-	}
+    @Operation(summary = "채팅방 생성", description = "채팅방을 생성합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "정상 처리"),
+    })
+    @PostMapping("")
+    public ResponseEntity<ChatRoomPostResponseDto> createChatRoom(
+            @RequestBody ChatRoomPostRequestDto request) {
+        ChatRoomPostResponseDto response = chatRoomFacadeService.createChatRoom(request);
+        return ResponseEntity.ok().body(response);
+    }
 
-	@Operation(summary = "채팅방 리스트 조회", description = "사용자의 채팅방 리스트를 조회합니다.")
-	@ApiResponses(value = {
-		@ApiResponse(responseCode = "200", description = "정상 처리"),
-	})
-	@GetMapping("")
-	public ResponseEntity<List<ChatRoomListGetResponseDto>> getChatRoomList() {
-		List<ChatRoomListGetResponseDto> responses = chatRoomFacadeService.getChatRoomList();
-		return ResponseEntity.ok().body(responses);
-	}
+    @Operation(summary = "채팅방 리스트 조회", description = "사용자의 채팅방 리스트를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "정상 처리"),
+    })
+    @GetMapping("")
+    public ResponseEntity<List<ChatRoomListGetResponseDto>> getChatRoomList() {
+        List<ChatRoomListGetResponseDto> responses = chatRoomFacadeService.getChatRoomList();
+        return ResponseEntity.ok().body(responses);
+    }
 
-	@Operation(summary = "채팅방 나가기", description = "채팅을 나갑니다.")
-	@ApiResponses(value = {
-		@ApiResponse(responseCode = "204", description = "정상 처리"),
-	})
-	@DeleteMapping("/{chatRoomId}/exit")
-	public ResponseEntity<List<ChatRoomListGetResponseDto>> deleteChatRoom(@PathVariable Integer chatRoomId) {
-		chatRoomFacadeService.leaveChatRoom(chatRoomId);
-		return ResponseEntity.noContent().build();
-	}
+    @Operation(summary = "채팅방 나가기", description = "채팅을 나갑니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "정상 처리"),
+    })
+    @DeleteMapping("/{chatRoomId}/exit")
+    public ResponseEntity<Void> deleteChatRoom(@PathVariable Integer chatRoomId) {
+        chatRoomFacadeService.leaveChatRoom(chatRoomId);
+        return ResponseEntity.noContent().build();
+    }
 }
