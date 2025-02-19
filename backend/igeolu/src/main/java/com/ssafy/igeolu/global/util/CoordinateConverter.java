5f94b038466f52a971d7e2dbdeacea1bfb4e3466
박진훈 <g22203@naver.com>
2025-02-20 01:28:31 +0900

    feat: 매물 수정시 좌표값 (x, y), (lat, long) 구분해서 반환 [S12P11D205-387]

--- backend/igeolu/src/main/java/com/ssafy/igeolu/global/util/CoordinateConverter.java
+++ backend/igeolu/src/main/java/com/ssafy/igeolu/global/util/CoordinateConverter.java
@@ -10,6 +10,11 @@ public class CoordinateConverter {
 
 	public static double[] convertToLatLon(double x, double y) {
 
+		// lat, long 으로 들어오면 그대로 반환
+		if (!isEPSG5179(x, y)) {
+			return new double[]{y, x};
+		}
+
 		CRSFactory crsFactory = new CRSFactory();
 		CoordinateReferenceSystem sourceCRS = crsFactory.createFromName("EPSG:5179");
 		CoordinateReferenceSystem targetCRS = crsFactory.createFromName("EPSG:4326");
@@ -23,4 +28,9 @@ public class CoordinateConverter {
 
 		return new double[] {targetCoord.y, targetCoord.x}; // [latitude, longitude]
 	}
+
+	private static boolean isEPSG5179(double x, double y) {
+		// EPSG:5179 (UTM-K) 범위
+        return (x >= 900000 && x <= 2000000) && (y >= 1800000 && y <= 2900000);
+    }
 }
