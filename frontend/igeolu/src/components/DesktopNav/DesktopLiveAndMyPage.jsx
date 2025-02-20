a4523aaa8768a2fb121e009bd770f964666cefaf
오승우 <osw9921@gmail.com>
2025-02-20 16:31:27 +0900

    fix/ 데스크탑 네브바 방찾기->원룸 바꿈[S12PqqD205-390]

--- frontend/igeolu/src/components/DesktopNav/DesktopLiveAndMyPage.jsx
+++ frontend/igeolu/src/components/DesktopNav/DesktopLiveAndMyPage.jsx
@@ -11,7 +11,7 @@ import NotificationApi from '../../services/NotificationApi';
 import { useUser } from '../../contexts/UserContext'; // useUser 추가
 
 const NAV_ITEMS = [
-  { id: 1, title: '방찾기', path: '/map?type=room' },
+  { id: 1, title: '원룸', path: '/map?type=room' },
   { id: 2, title: '공인중개사', path: '/map?type=agent' },
   { id: 3, title: '라이브', path: '/live-join' },
 ];
